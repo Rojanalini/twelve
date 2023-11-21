6e81bddfe153a6c89388a291c3aa46b5227c1b0d
Rojanalini <javvajirojanalini1234@gmail.com>
2023-11-21 20:44:50 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def sudheer(request):
+    return render(request,'sudheer.html')
+
+
+
+  def srujana(request):
+    return render(request,'srujana.html')
+  
